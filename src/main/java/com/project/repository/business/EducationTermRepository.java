29780aa5749af42a64c5085685f9df660929e772
Muhammed Emin Kaba <m.eminkaba0000@gmail.com>
2023-09-28 00:49:07 +0300

    Last Update

---
+++ src/main/java/com/project/repository/business/EducationTermRepository.java
@@ -0,0 +1,7 @@
+package com.project.repository.business;
+
+import com.project.entity.business.EducationTerm;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EducationTermRepository extends JpaRepository<EducationTerm, Long> {
+}
